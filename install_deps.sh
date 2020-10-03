78b0f8fcc2cb2f403755a773c0a5e19d6238bd99
dekashio <9784097+dekashio@users.noreply.github.com>
2020-10-03 23:29:00 +0300

    Create install_deps.sh
    Created install deps file to make installation process simpler.

---
+++ install_deps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+INSTALL_DIR=$(pwd)/
+
+#install os deps:
+apt install python3-pip git libcurl4-openssl-dev libssl-dev make -qq -y
+#get tools sources from github, compile them  and install to PATH:
+git clone https://github.com/hashcat/hashcat-utils
+cd hashcat-utils/src/
+make -j $(nproc)
+cp cap2hccapx.bin /usr/local/bin/
+cd $INSTALL_DIR
+git clone https://github.com/ZerBea/hcxdumptool
+cd hcxdumptool
+make -j $(nproc)
+make install
+cd $INSTALL_DIR
+git clone https://github.com/ZerBea/hcxtools
+cd hcxtools
+make -j $(nproc)
+make install
+cd $INSTALL_DIR
+#get python3 deps:
+pip3 install dropbox pyshark
