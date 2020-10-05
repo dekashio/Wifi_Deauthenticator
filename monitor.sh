4a4a355b3855fa644bb2350cca98be53a7100c9d
dekashio <9784097+dekashio@users.noreply.github.com>
2020-10-05 08:42:17 +0300

    Update monitor.sh
    Changed final results.

--- monitor.sh
+++ monitor.sh
@@ -13,7 +13,7 @@ ip link set $device up
 state=$(iw dev $device info | grep type | cut -d " " -f 2)
 state=$(echo -e "${state}" | tr -d '[:space:]')
 if [ "$state" == "monitor" ]; then
-	echo $device "in monitor mode"
+	echo "succesfully turned $device to monitor mode"
 else
-	echo $device "not in monitor mode"
+	echo "Failed to put $device in monitor mode"
 fi
