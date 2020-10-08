69d9325ff8a280b4be4cdde73302f21ad694852e
dekashio <9784097+dekashio@users.noreply.github.com>
2020-10-08 22:34:39 +0300

    Update monitor.sh
    typo

--- monitor.sh
+++ monitor.sh
@@ -3,7 +3,7 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-echo "Enter WiFi device name to put in monitor mode: "
+echo "Enter WiFi device name to put into monitor mode: "
 read device
 systemctl stop wpa_supplicant.service
 systemctl mask wpa_supplicant.service
