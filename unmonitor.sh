56587070c159a5a4281e377c139e8c60542e1e46
dekashio <9784097+dekashio@users.noreply.github.com>
2020-10-08 22:25:42 +0300

    Update unmonitor.sh
    changed Wifi device state check method and changed the order of wpa_supplicant restart.

--- unmonitor.sh
+++ unmonitor.sh
@@ -5,13 +5,12 @@ if [ "$EUID" -ne 0 ]
 fi
 echo "Enter WiFi device name to put in managed mode: "
 read device
-systemctl unmask wpa_supplicant.service
-systemctl restart wpa_supplicant.service
 ip link set $device down
 iw dev $device set type managed
 ip link set $device up
-state=$(iw dev $device info | grep type | cut -d " " -f 2)
-state=$(echo -e "${state}" | tr -d '[:space:]')
+systemctl unmask wpa_supplicant.service
+systemctl restart wpa_supplicant.service
+state=$(iw dev $device info | grep type | awk '{ print $2 }' | tr -d '[:space:]')
 if [ "$state" == "managed" ]; then
 	echo "Successfully put $device in managed mode"
 else
